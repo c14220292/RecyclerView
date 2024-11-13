8132cabfc137c4c9682ebba8a0d4f76f85580fdd
Nama Anda <hermawanedbert@gmail.com>
2024-11-13 22:36:07 +0700

    Commit 7

--- app/src/main/java/nit2x/paba/recyclerview/adapterRecView.kt
+++ app/src/main/java/nit2x/paba/recyclerview/adapterRecView.kt
@@ -4,13 +4,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.app.ComponentActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
 class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
@@ -20,6 +18,7 @@ class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
 
     interface OnItemClickCallback {
         fun onItemClicked(data:wayang)
+        fun delData(pos: Int)
     }
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -27,6 +26,7 @@ class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
             var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
             var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
             var _gambarWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
+            var _btnHapus = itemView.findViewById<Button>(R.id.btnHapus)
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -53,6 +53,10 @@ class adapterRecView (private val listWayang: ArrayList<wayang>) : RecyclerView
         holder._gambarWayang.setOnClickListener {
 //            Toast.makeText(holder.itemView.context,wayang.nama,Toast.LENGTH_LONG).show()
             onItemClickCallback.onItemClicked(listWayang[position])
+        }
+
+        holder._btnHapus.setOnClickListener {
+            onItemClickCallback.delData(position)
         }
     }
 
